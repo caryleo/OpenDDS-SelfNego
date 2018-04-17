6cedcf32668404455efecd0814932e675bf19912
JustianPupil <Justian@protonmail.com>
2018-04-17 11:01:28 +0800

    网络参数获取，先新建一个Netstate对象，调用test()方法，完成后可以通过getBandWidth、getDelay、getPacketLoss等方法获取带宽、时延、丢包率等参数，目前吞吐量和带宽利用率不能使用

--- Getsysinfo.cpp
+++ Getsysinfo.cpp
@@ -1,3 +1,4 @@
+#include "StdAfx.h"  
 #include "GetsysInfo.h"  
 #include <atlbase.h>  
 #include <atlstr.h>
@@ -12,337 +13,9 @@ GetSysInfo::~GetSysInfo(void)
 {
 }
 
-void GetSysInfo::GetOSVersion(CString &strOSVersion, CString &strServiceVersion)
-{
-	CString str;
-	OSVERSIONINFOEX osvi;
-	SYSTEM_INFO si;
-	BOOL bOsVersionInfoEx;
 
-	ZeroMemory(&si, sizeof(SYSTEM_INFO));
-	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-
-	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-	if (!(bOsVersionInfoEx = GetVersionEx((OSVERSIONINFO *)&osvi)))
-	{
-		osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-		GetVersionEx((OSVERSIONINFO *)&osvi);
-	}
-
-
-	GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")),
-		"GetNativeSystemInfo");
-
-	GetSystemInfo(&si);
-	switch (osvi.dwPlatformId)
-	{
-	case VER_PLATFORM_WIN32_NT:
-		if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 0)
-		{
-			if (osvi.wProductType == VER_NT_WORKSTATION)
-			{
-				str.Format(_T("Windows Vista "));
-			}
-			else
-			{
-				str.Format(_T("Windows Server \"Longhorn\" "));
-			}
-		}
-		if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 2)
-		{
-			if (GetSystemMetrics(SM_SERVERR2))
-			{
-				str.Format(_T("Microsoft Windows Server 2003 \"R2\" "));
-			}
-			else if (osvi.wProductType == VER_NT_WORKSTATION &&
-				si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
-			{
-				str.Format(_T("Microsoft Windows XP Professional x64 Edition "));
-			}
-			else
-			{
-				str.Format(_T("Microsoft Windows Server 2003, "));
-			}
-		}
-
-		if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 1)
-		{
-			str.Format(_T("Microsoft Windows XP "));
-		}
-
-		if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0)
-			str.Format(_T("Microsoft Windows 2000 "));
-
-		if (osvi.dwMajorVersion <= 4)
-		{
-			str.Format(_T("Microsoft Windows NT "));
-		}
-
-		// Test for specific product on Windows NT 4.0 SP6 and later.  
-		if (bOsVersionInfoEx)
-		{
-
-			//��Service Pack �汾����  
-			strServiceVersion.Format(_T("Service Pack %d"), osvi.wServicePackMajor);
-
-			// Test for the workstation type.  
-			if (osvi.wProductType == VER_NT_WORKSTATION &&
-				si.wProcessorArchitecture != PROCESSOR_ARCHITECTURE_AMD64)
-			{
-				if (osvi.dwMajorVersion == 4)
-					str = str + _T("Workstation 4.0");
-				else if (osvi.wSuiteMask & VER_SUITE_PERSONAL)
-					str = str + _T("Home Edition");
-				else str = str + _T("Professional");
-			}
-
-			// Test for the server type.  
-			else if (osvi.wProductType == VER_NT_SERVER ||
-				osvi.wProductType == VER_NT_DOMAIN_CONTROLLER)
-			{
-				if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 2)
-				{
-					if (si.wProcessorArchitecture ==
-						PROCESSOR_ARCHITECTURE_IA64)
-					{
-						if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-							str = str + _T("Datacenter Edition for Itanium-based Systems");
-						else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-							str = str + _T("Enterprise Edition for Itanium-based Systems");
-					}
-
-					else if (si.wProcessorArchitecture ==
-						PROCESSOR_ARCHITECTURE_AMD64)
-					{
-						if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-							str = str + _T("Datacenter x64 Edition ");
-						else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-							str = str + _T("Enterprise x64 Edition ");
-						else str = str + _T("Standard x64 Edition ");
-					}
-
-					else
-					{
-						if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-							str = str + _T("Datacenter Edition ");
-						else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-							str = str + _T("Enterprise Edition ");
-						else if (osvi.wSuiteMask & VER_SUITE_BLADE)
-							str = str + _T("Web Edition ");
-						else str = str + _T("Standard Edition ");
-					}
-				}
-				else if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0)
-				{
-					if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-						str = str + _T("Datacenter Server ");
-					else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-						str = str + _T("Advanced Server ");
-					else str = str + _T("Server ");
-				}
-				else  // Windows NT 4.0   
-				{
-					if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-						str = str + _T("Server 4.0, Enterprise Edition ");
-					else str = str + _T("Server 4.0 ");
-				}
-			}
-		}
-		// Test for specific product on Windows NT 4.0 SP5 and earlier  
-		else
-		{
-			HKEY hKey;
-			TCHAR szProductType[256];
-			DWORD dwBufLen = 256 * sizeof(TCHAR);
-			LONG lRet;
-
-			lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-				_T("SYSTEM\\CurrentControlSet\\Control\\ProductOptions"), 0, KEY_QUERY_VALUE, &hKey);
-			if (lRet != ERROR_SUCCESS)
-				strOSVersion = str;
-			return;
-
-			lRet = RegQueryValueEx(hKey, TEXT("ProductType"),
-				NULL, NULL, (LPBYTE)szProductType, &dwBufLen);
-			RegCloseKey(hKey);
-
-			if ((lRet != ERROR_SUCCESS) ||
-				(dwBufLen > 256 * sizeof(TCHAR)))
-				strOSVersion = str;
-			return;
-
-			if (lstrcmpi(TEXT("WINNT"), szProductType) == 0)
-				str = str + _T("Workstation ");
-			if (lstrcmpi(TEXT("LANMANNT"), szProductType) == 0)
-				str = str + _T("Server ");
-			if (lstrcmpi(TEXT("SERVERNT"), szProductType) == 0)
-				str = str + _T("Advanced Server ");
-			str.Format(_T("%d.%d "), osvi.dwMajorVersion, osvi.dwMinorVersion);
-		}
-
-		// Display service pack (if any) and build number.  
-
-		if (osvi.dwMajorVersion == 4 &&
-			lstrcmpi(osvi.szCSDVersion, TEXT("Service Pack 6")) == 0)
-		{
-			HKEY hKey;
-			LONG lRet;
-
-			// Test for SP6 versus SP6a.  
-			lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-				_T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Hotfix\\Q246009"), 0, KEY_QUERY_VALUE, &hKey);
-			if (lRet == ERROR_SUCCESS)
-				str.Format(_T("Service Pack 6a (Build %d)\n"),
-					osvi.dwBuildNumber & 0xFFFF);
-			else // Windows NT 4.0 prior to SP6a  
-			{
-				_tprintf(TEXT("%s (Build %d)\n"),
-					osvi.szCSDVersion,
-					osvi.dwBuildNumber & 0xFFFF);
-			}
-
-			RegCloseKey(hKey);
-		}
-		else // not Windows NT 4.0   
-		{
-			_tprintf(TEXT("%s (Build %d)\n"),
-				osvi.szCSDVersion,
-				osvi.dwBuildNumber & 0xFFFF);
-		}
-
-		break;
-
-		// Test for the Windows Me/98/95.  
-	case VER_PLATFORM_WIN32_WINDOWS:
-
-		if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 0)
-		{
-			str.Format(_T("Microsoft Windows 95 "));
-			if (osvi.szCSDVersion[1] == 'C' || osvi.szCSDVersion[1] == 'B')
-				str = str + _T("OSR2 ");
-		}
-
-		if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 10)
-		{
-			str.Format(_T("Microsoft Windows 98 "));
-			if (osvi.szCSDVersion[1] == 'A' || osvi.szCSDVersion[1] == 'B')
-				str = str + _T("SE ");
-		}
-		if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 90)
-		{
-			str.Format(_T("Microsoft Windows Millennium Edition\n"));
-		}
-		break;
-
-	case VER_PLATFORM_WIN32s:
-		str.Format(_T("Microsoft Win32s\n"));
-		break;
-	default:
-		break;
-	}
-
-	strOSVersion = str;
-}
-
-BOOL GetSysInfo::IsWow64()
-{
-	typedef BOOL(WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
-	LPFN_ISWOW64PROCESS fnIsWow64Process;
-	BOOL bIsWow64 = FALSE;
-	fnIsWow64Process = (LPFN_ISWOW64PROCESS)GetProcAddress(GetModuleHandle(_T("kernel32")), "IsWow64Process");
-	if (NULL != fnIsWow64Process)
-	{
-		fnIsWow64Process(GetCurrentProcess(), &bIsWow64);
-	}
-	return bIsWow64;
-}
-
-void GetSysInfo::GetCpuInfo(CString &chProcessorName, CString &chProcessorType, DWORD &dwNum, DWORD &dwMaxClockSpeed)
-{
-
-	CString strPath = _T("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");//ע����Ӽ�·��  
-	CRegKey regkey;//����ע��������  
-	LONG lResult;//LONG�ͱ�������Ӧ���  
-	lResult = regkey.Open(HKEY_LOCAL_MACHINE, LPCTSTR(strPath), KEY_ALL_ACCESS); //��ע����  
-	if (lResult != ERROR_SUCCESS)
-	{
-		return;
-	}
-	WCHAR chCPUName[50] = { 0 };
-	DWORD dwSize = 50;
-
-	//��ȡProcessorNameString�ֶ�ֵ  
-	if (ERROR_SUCCESS == regkey.QueryStringValue(_T("ProcessorNameString"), chCPUName, &dwSize))
-	{
-		chProcessorName = chCPUName;
-	}
-
-	//��ѯCPU��Ƶ  
-	DWORD dwValue;
-	if (ERROR_SUCCESS == regkey.QueryDWORDValue(_T("~MHz"), dwValue))
-	{
-		dwMaxClockSpeed = dwValue;
-	}
-	regkey.Close();//�ر�ע���  
-				   //UpdateData(FALSE);  
-
-				   //��ȡCPU������Ŀ  
-	SYSTEM_INFO si;
-	memset(&si, 0, sizeof(SYSTEM_INFO));
-	GetSystemInfo(&si);
-	dwNum = si.dwNumberOfProcessors;
-
-	switch (si.dwProcessorType)
-	{
-	case PROCESSOR_INTEL_386:
-	{
-		chProcessorType.Format(_T("Intel 386 processor"));
-	}
-	break;
-	case PROCESSOR_INTEL_486:
-	{
-		chProcessorType.Format(_T("Intel 486 Processor"));
-	}
-	break;
-	case PROCESSOR_INTEL_PENTIUM:
-	{
-		chProcessorType.Format(_T("Intel Pentium Processor"));
-	}
-	break;
-	case PROCESSOR_INTEL_IA64:
-	{
-		chProcessorType.Format(_T("Intel IA64 Processor"));
-	}
-	break;
-	case PROCESSOR_AMD_X8664:
-	{
-		chProcessorType.Format(_T("AMD X8664 Processor"));
-	}
-	break;
-	default:
-		chProcessorType.Format(_T("δ֪"));
-		break;
-	}
-
-	//GetDisplayName()  
-}
-
-void  GetSysInfo::GetMemoryInfo(CString &dwTotalPhys, CString &dwTotalVirtual)
-{
-	//   TODO:     Add   extra   initialization   here   
-	MEMORYSTATUS   Mem;
-	//   get   the   memory   status   
-	GlobalMemoryStatus(&Mem);
-
-	DWORD dwSize = (DWORD)Mem.dwTotalPhys / (1024 * 1024);
-	DWORD dwVirtSize = (DWORD)Mem.dwTotalVirtual / (1024 * 1024);
-
-	dwTotalPhys.Format(_T("�����ڴ�:%ld MB"), dwSize);
-	dwTotalVirtual.Format(_T("�����ڴ�:%ld MB"), dwVirtSize);
-}
-
-int GetSysInfo::GetInterFaceCount()
-{
+//int GetSysInfo::GetInterFaceCount(struct interfaces *strs)
+int GetSysInfo::GetInterFaceCount(long *band) {
 	/*CGetNetData pNet;
 	DWORD pCount = pNet.GetNetworkInterfacesCount();
 	return pCount;*/
@@ -359,24 +32,24 @@ int GetSysInfo::GetInterFaceCount()
 
 		char s_key[4096];
 		sprintf_s(s_key, 4096, "510");
-		//RegQueryValueEx�Ĺ̶����ø�ʽ          
+		//RegQueryValueEx的固定调用格式          
 		CString str(s_key);
 
-		//���RegQueryValueEx����ִ��ʧ�������ѭ��  
+		//如果RegQueryValueEx函数执行失败则进入循环  
 		while ((ret = RegQueryValueEx(HKEY_PERFORMANCE_DATA, str, 0, &type, data, &size)) != ERROR_SUCCESS)
 		{
 			Sleep(10);
-			//���RegQueryValueEx�ķ���ֵΪERROR_MORE_DATA(������ڴ���data̫С����������RegQueryValueEx���ص�����)  
+			//如果RegQueryValueEx的返回值为ERROR_MORE_DATA(申请的内存区data太小，不能容纳RegQueryValueEx返回的数据)  
 			if (ret == ERROR_MORE_DATA)
 			{
 				Sleep(10);
 				size += DEFAULT_BUFFER_SIZE;
-				data = (unsigned char*)realloc(data, size);//���·����㹻����ڴ�  
+				data = (unsigned char*)realloc(data, size);//重新分配足够大的内存  
 
-				ret = RegQueryValueEx(HKEY_PERFORMANCE_DATA, str, 0, &type, data, &size);//����ִ��RegQueryValueEx����  
+				ret = RegQueryValueEx(HKEY_PERFORMANCE_DATA, str, 0, &type, data, &size);//重新执行RegQueryValueEx函数  
 			}
-			//���RegQueryValueEx����ֵ�Ծ�δ�ɹ���������.....(ע���ڴ�й¶��free������~~~)��  
-			//���if��֤�����whileֻ�ܽ���һ��~~~������ѭ��  
+			//如果RegQueryValueEx返回值仍旧未成功则函数返回.....(注意内存泄露“free函数”~~~)。  
+			//这个if保证了这个while只能进入一次~~~避免死循环  
 			if (ret != ERROR_SUCCESS)
 			{
 				if (NULL != data)
@@ -384,14 +57,14 @@ int GetSysInfo::GetInterFaceCount()
 					free(data);
 					data = NULL;
 				}
-				return 0;//0���ӿ�  
+				return 0;//0个接口  
 			}
 		}
 
-		//����ִ�гɹ�֮����ǶԷ��ص�data�ڴ������ݵĽ����ˣ��������ȥ�鿴MSDN�й�RegQueryValueEx�����������ݽṹ��˵��  
-		//�õ����ݿ�       
+		//函数执行成功之后就是对返回的data内存中数据的解析了，这个建议去查看MSDN有关RegQueryValueEx函数参数数据结构的说明  
+		//得到数据块       
 		PERF_DATA_BLOCK  *dataBlockPtr = (PERF_DATA_BLOCK *)data;
-		//�õ���һ������  
+		//得到第一个对象  
 		PERF_OBJECT_TYPE *objectPtr = (PERF_OBJECT_TYPE *)((BYTE *)dataBlockPtr + dataBlockPtr->HeaderLength);
 
 		for (int a = 0; a<(int)dataBlockPtr->NumObjectTypes; a++)
@@ -432,9 +105,18 @@ int GetSysInfo::GetInterFaceCount()
 					DWORD bandwith = *((DWORD *)((BYTE *)counterBlockPtr + processIdOffset));
 					DWORD tottraff = 0;
 
-					Interfaces.AddTail(CString(pName)); //������������  
-					Bandwidths.AddTail(bandwith);       //����  
-					TotalTraffics.AddTail(tottraff);    // ������ʼ��Ϊ0  
+					/*printf("%s\n", pName);
+					printf("%ld\n", bandwith);
+					printf("***************************************************");
+					strcmp(pName, strs->name[b]);
+					strs->band[b] = bandwith;
+					printf("%s\n", pName);
+					printf("%ld\n", bandwith);
+					printf("***************************************************");*/
+					band[b] = bandwith;
+					Interfaces.AddTail(CString(pName)); //各网卡的名称  
+					Bandwidths.AddTail(bandwith);       //带宽  
+					TotalTraffics.AddTail(tottraff);    // 流量初始化为0  
 
 					PERF_COUNTER_BLOCK  *pCtrBlk = (PERF_COUNTER_BLOCK *)((BYTE *)instancePtr + instancePtr->ByteLength);
 
@@ -476,216 +158,4 @@ void GetSysInfo::GetInterFaceName(CString &InterfaceName, int pNum)
 	str.Format(_T("%d"), dwBandwidth);
 
 	InterfaceName = InterfaceName + str;
-}
-
-void GetSysInfo::GetDiskInfo(DWORD &dwNum, CString chDriveInfo[])
-{
-	DWORD DiskCount = 0;
-
-	//����GetLogicalDrives()�������Ի�ȡϵͳ���߼����������������������ص���һ��32λ�޷����������ݡ�  
-	DWORD DiskInfo = GetLogicalDrives();
-
-	//ͨ��ѭ�������鿴ÿһλ�����Ƿ�Ϊ1�����Ϊ1�����Ϊ��,���Ϊ0����̲����ڡ�  
-	while (DiskInfo)
-	{
-		//ͨ��λ������߼���������ж��Ƿ�Ϊ1  
-		Sleep(10);
-		if (DiskInfo & 1)
-		{
-			DiskCount++;
-		}
-		DiskInfo = DiskInfo >> 1;//ͨ��λ��������Ʋ�����֤ÿѭ��һ��������λ�������ƶ�һλ��*/  
-	}
-
-	if (dwNum < DiskCount)
-	{
-		return;//ʵ�ʵĴ�����Ŀ����dwNum  
-	}
-	dwNum = DiskCount;//�����̷�����������  
-
-
-					  //-------------------------------------------------------------------//  
-					  //ͨ��GetLogicalDriveStrings()������ȡ�����������ַ�����Ϣ����  
-	int DSLength = GetLogicalDriveStrings(0, NULL);
-
-	WCHAR* DStr = new WCHAR[DSLength];
-	memset(DStr, 0, DSLength);
-
-	//ͨ��GetLogicalDriveStrings���ַ�����Ϣ���Ƶ�����������,���б�������������������Ϣ��  
-	GetLogicalDriveStrings(DSLength, DStr);
-
-	int DType;
-	int si = 0;
-	BOOL fResult;
-	unsigned _int64 i64FreeBytesToCaller;
-	unsigned _int64 i64TotalBytes;
-	unsigned _int64 i64FreeBytes;
-
-	//��ȡ����������Ϣ������DStr�ڲ����ݸ�ʽ��A:\NULLB:\NULLC:\NULL������DSLength/4���Ի�þ����ѭ����Χ  
-	for (int i = 0; i<DSLength / 4; ++i)
-	{
-		Sleep(10);
-		CString strdriver = DStr + i * 4;
-		CString strTmp, strTotalBytes, strFreeBytes;
-		DType = GetDriveType(strdriver);//GetDriveType���������Ի�ȡ���������ͣ�����Ϊ�������ĸ�Ŀ¼  
-		switch (DType)
-		{
-		case DRIVE_FIXED:
-		{
-			strTmp.Format(_T("���ش���"));
-		}
-		break;
-		case DRIVE_CDROM:
-		{
-			strTmp.Format(_T("DVD������"));
-		}
-		break;
-		case DRIVE_REMOVABLE:
-		{
-			strTmp.Format(_T("���ƶ�����"));
-		}
-		break;
-		case DRIVE_REMOTE:
-		{
-			strTmp.Format(_T("�������"));
-		}
-		break;
-		case DRIVE_RAMDISK:
-		{
-			strTmp.Format(_T("����RAM����"));
-		}
-		break;
-		case DRIVE_UNKNOWN:
-		{
-			strTmp.Format(_T("����RAMδ֪�豸"));
-		}
-		break;
-		default:
-			strTmp.Format(_T("δ֪�豸"));
-			break;
-		}
-
-		//GetDiskFreeSpaceEx���������Ի�ȡ���������̵Ŀռ�״̬,�������ص��Ǹ�BOOL��������  
-		fResult = GetDiskFreeSpaceEx(strdriver,
-			(PULARGE_INTEGER)&i64FreeBytesToCaller,
-			(PULARGE_INTEGER)&i64TotalBytes,
-			(PULARGE_INTEGER)&i64FreeBytes);
-
-		if (fResult)
-		{
-			strTotalBytes.Format(_T("����������%fMB"), (float)i64TotalBytes / 1024 / 1024);
-			strFreeBytes.Format(_T("����ʣ��ռ�%fMB"), (float)i64FreeBytesToCaller / 1024 / 1024);
-		}
-		else
-		{
-			strTotalBytes.Format(_T(""));
-			strFreeBytes.Format(_T(""));
-		}
-		chDriveInfo[i] = strTmp + _T("(") + strdriver + _T("):") + strTotalBytes + strFreeBytes;
-		si += 4;
-	}
-}
-
-void GetSysInfo::GetDisplayCardInfo(DWORD &dwNum, CString chCardName[])
-{
-	HKEY keyServ;
-	HKEY keyEnum;
-	HKEY key;
-	HKEY key2;
-	LONG lResult;//LONG�ͱ��������溯������ֵ  
-
-				 //��ѯ"SYSTEM\\CurrentControlSet\\Services"�µ������Ӽ����浽keyServ  
-	lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\CurrentControlSet\\Services"), 0, KEY_READ, &keyServ);
-	if (ERROR_SUCCESS != lResult)
-		return;
-
-
-	//��ѯ"SYSTEM\\CurrentControlSet\\Enum"�µ������Ӽ����浽keyEnum  
-	lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\CurrentControlSet\\Enum"), 0, KEY_READ, &keyEnum);
-	if (ERROR_SUCCESS != lResult)
-		return;
-
-	int i = 0, count = 0;
-	DWORD size = 0, type = 0;
-	for (;; ++i)
-	{
-		Sleep(5);
-		size = 512;
-		TCHAR name[512] = { 0 };//����keyServ�¸�������ֶ�����  
-
-								//���ö��keyServ�µĸ������ֶα��浽name��  
-		lResult = RegEnumKeyEx(keyServ, i, name, &size, NULL, NULL, NULL, NULL);
-
-		//Ҫ��ȡ��������ڣ���keyServ������ȫ��������ʱ����ѭ��  
-		if (lResult == ERROR_NO_MORE_ITEMS)
-			break;
-
-		//��keyServ�������ֶ�Ϊname����ʶ���ֶε�ֵ���浽key  
-		lResult = RegOpenKeyEx(keyServ, name, 0, KEY_READ, &key);
-		if (lResult != ERROR_SUCCESS)
-		{
-			RegCloseKey(keyServ);
-			return;
-		}
-
-
-		size = 512;
-		//��ѯkey�µ��ֶ�ΪGroup���Ӽ��ֶ������浽name  
-		lResult = RegQueryValueEx(key, TEXT("Group"), 0, &type, (LPBYTE)name, &size);
-		if (lResult == ERROR_FILE_NOT_FOUND)
-		{
-			//?��������  
-			RegCloseKey(key);
-			continue;
-		};
-
-
-
-		//�����ѯ����name����Video��˵���ü������Կ�������  
-		if (_tcscmp(TEXT("Video"), name) != 0)
-		{
-			RegCloseKey(key);
-			continue;     //����forѭ��  
-		};
-
-		//��������������ִ�еĻ�˵���Ѿ��鵽���й��Կ�����Ϣ������������Ĵ���ִ����֮��Ҫbreak��һ��forѭ������������  
-		lResult = RegOpenKeyEx(key, TEXT("Enum"), 0, KEY_READ, &key2);
-		RegCloseKey(key);
-		key = key2;
-		size = sizeof(count);
-		lResult = RegQueryValueEx(key, TEXT("Count"), 0, &type, (LPBYTE)&count, &size);//��ѯCount�ֶΣ��Կ���Ŀ��  
-
-		dwNum = count;//�����Կ���Ŀ  
-		for (int j = 0; j <count; ++j)
-		{
-			TCHAR sz[512] = { 0 };
-			TCHAR name[64] = { 0 };
-			wsprintf(name, TEXT("%d"), j);
-			size = sizeof(sz);
-			lResult = RegQueryValueEx(key, name, 0, &type, (LPBYTE)sz, &size);
-
-
-			lResult = RegOpenKeyEx(keyEnum, sz, 0, KEY_READ, &key2);
-			if (ERROR_SUCCESS)
-			{
-				RegCloseKey(keyEnum);
-				return;
-			}
-
-
-			size = sizeof(sz);
-			lResult = RegQueryValueEx(key2, TEXT("FriendlyName"), 0, &type, (LPBYTE)sz, &size);
-			if (lResult == ERROR_FILE_NOT_FOUND)
-			{
-				size = sizeof(sz);
-				lResult = RegQueryValueEx(key2, TEXT("DeviceDesc"), 0, &type, (LPBYTE)sz, &size);
-				chCardName[j] = sz;//�����Կ�����  
-			};
-			RegCloseKey(key2);
-			key2 = NULL;
-		};
-		RegCloseKey(key);
-		key = NULL;
-		break;
-	}
 }
--- NetState.cpp
+++ NetState.cpp
@@ -13,29 +13,72 @@ NetState::~NetState(void)
 {
 }
 
-void NetState::testBandwidth()
+//void NetState::testBandwidth()
+//{
+//
+//}
+//
+//void NetState::testRatio()
+//{
+//
+//}
+//
+//void NetState::testDelay()
+//{
+//
+//}
+//
+//void NetState::testThroughtput()
+//{
+//
+//}
+//
+//void NetState::testPacketLoss()
+//{
+//
+//}
+void NetState::test(char ip[])
 {
+	long bands[10];
+	GetSysInfo info;
+	int num = info.GetInterFaceCount(bands);
+	//printf("%d\n", num);
+	int best = 0;
+	for (int i = 0; i < num; i++) {
+		if (bands[i] > bands[best])
+			best = i;
+	}
+	setBandWidth(bands[best]);
 
-}
+	CPing objPing;
 
-void NetState::testRatio()
-{
+	char *szDestIP;
+	char defaultip[100] = "10.4.21.55";
+	szDestIP = ip;
+	if (ip == NULL)
+		szDestIP = defaultip;
+	PingReply reply;
 
-}
-
-void NetState::testDelay()
-{
-
-}
-
-void NetState::testThroughtput()
-{
-
-}
-
-void NetState::testPacketLoss()
-{
-
+	//printf("Pinging %s with %d bytes of data:\n", szDestIP, DEF_PACKET_SIZE);
+	double times = 0;
+	int rolls = 100;
+	int faildpackegs = 0;
+	for (int i = 0; i < rolls; i++)
+	{
+		objPing.Ping(szDestIP, &reply, (DWORD)10);
+		if (reply.m_dwRoundTripTime > 50) {
+			faildpackegs++;
+			continue;
+		}
+		times += reply.m_dwRoundTripTime;
+		printf("\r[*]Network Testing...%d%%", i);
+		//printf("Reply from %s: bytes=%ld time=%ldms TTL=%ld\n", szDestIP, reply.m_dwBytes, reply.m_dwRoundTripTime, reply.m_dwTTL);
+		//Sleep(500);
+	}
+	setDelay(times / (double)(rolls - faildpackegs));
+	setPacketLoss(((double)faildpackegs / rolls) * 100.0);
+	setThrouhgput(DEF_PACKET_SIZE / avrtime);
+	setRatio(throuhgpu / bandWidth);
 }
 
 void NetState::setBandWidth(double input)
