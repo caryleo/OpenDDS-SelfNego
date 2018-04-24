ee062d3bb981700acb64ac1c9ac40c4394992016
JustianPupil <Justian@protonmail.com>
2018-04-24 10:33:38 +0800

    Qos-SelfNego

--- SelfNego.cpp
+++ SelfNego.cpp
@@ -35,34 +35,56 @@ void SelfNego::getQoS()
         {
             if  (isReader == 1)
             {
-                reader.history.kind = KEEP_ALL_HISTORY_QOS;
+                writer.history.kind = KEEP_ALL_HISTORY_QOS;
             }
             else if (isReader == 2)
             {
-                writer.reliability.kind = RELIABLE_RELIABILITY_QOS;
-                writer.history.kind = KEEP_ALL_HISTORY_QOS;
+                reader.reliability.kind = RELIABLE_RELIABILITY_QOS;
+                reader.history.kind = KEEP_ALL_HISTORY_QOS;
             }
         }
         else if (packetloss > PACKETLOSS_THRESHOLD_2 && packetloss <= PACKETLOSS_THRESHOLD_3)
         {
             if  (isReader == 1)
             {
-                
+				reader.history. kind = KEEP_LAST_HISTORY_QOS;
+				reader.history.depth=10;
+				reader.deadline.period.sec = 150;
+				reader.deadline.period.nanosec = 0;
+				reader.time_based-filter .minimum_separation.sec=20;
+				reader.time_based-filter .minimum_separation.naosec=0;
             }
             else if (isReader == 2)
             {
-
+				writer.reliaability.kind = BEST_EFFORT_RELIABILITY_QOS;
+				writer.history. kind = KEEP_LAST_HISTORY_QOS;
+				writer.history.depth=10;
+				writer.deadline.period.sec = 100;
+				writer.deadline.period.nanosec = 0;
+				writer.lifespan.duration.sec = 50;
+				writer.lifespan.duration.naosec = 0;
             }
         }
         else
         {
             if  (isReader == 1)
             {
-
+				reader.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				reader.deadline.period.sec = 250;
+				reader.deadline.period.nanosec = 0;
+				reader.time_based-filter .minimum_separation.sec=50;
+				reader.time_based-filter .minimum_separation.naosec=0;
             }
             else if (isReader == 2)
             {
-
+				writer.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				writer.reliability.max_blocking_time.sec = 10;
+				writer.reliability.max_blocking_time.nanosec = 0;
+				writer.resource_limits.max_samples_per_instance = 100;
+				writer.deadline.period.sec = 200;
+				writer.deadline.period.nanosec = 0;
+				writer.lifespan.duration.sec = 20;
+				writer.lifespan.duration.naosec = 0;
             }
         }
     }
@@ -72,33 +94,68 @@ void SelfNego::getQoS()
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind =KEEP_ALL_HISTORY_QOS;
+				reader.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				reader.reliability.max_blocking_time.sec = 10;
+				reader.reliability.max_blocking_time.nanosec = 0;
+				reader.resource_limits.max_samples_per_instance = 100;
+				reader.deadline.period.sec     = 150;
+				reader.deadline.period.nanosec = 0;
             }
             else if (isReader == 2)
             {
-
+               writer.history.kind = KEEP_ALL_HISTORY_QOS; 
+			   writer.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+			   writer.reliability.max_blocking_time.sec = 10;
+			   writer.reliability.max_blocking_time.nanosec = 0;
+			   writer.resource_limits.max_samples_per_instance = 100;
+			   writer.deadline.period.sec = 100;
+			   writer.deadline.period.nanosec = 0;
             }
         }
         else if (packetloss > PACKETLOSS_THRESHOLD_2 && packetloss <= PACKETLOSS_THRESHOLD_3)
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind = KEEP_LAST_HISTORY_QOS;
+				reader.history.depth=10;
+				reader.deadline.period.sec = 200;
+				reader.deadline.period.nanosec = 0;
+				reader.ime_based-filter .minimum_separation.sec=20;
+				reader.time_based-filter .minimum_separation.naosec=0;
             }
             else if (isReader == 2)
             {
-
+				writer.history. kind = KEEP_LAST_HISTORY_QOS;
+				writer.history.depth=10;
+				writer.reliaability.kind = BEST_EFFORT_RELIABILITY_QOS;
+				writer.deadline.period.sec = 150;
+				writer.deadline.period.nanosec = 0;
+				writer.lifespan.duration.sec = 100;
+				writer.lifespan.duration.naosec = 0;
             }
         }
         else
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind =KEEP_ALL_HISTORY_QOS;
+				reader.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				reader.reliability.max_blocking_time.sec = 10;
+				reader.reliability.max_blocking_time.nanosec = 0;
+				reader.resource_limits.max_samples_per_instance = 100;
+				reader.deadline.period.sec     = 150;
+				reader.deadline.period.nanosec = 0;
             }
             else if (isReader == 2)
             {
-
+               writer.history.kind = KEEP_ALL_HISTORY_QOS; 
+			   writer.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+			   writer.reliability.max_blocking_time.sec = 10;
+			   writer.reliability.max_blocking_time.nanosec = 0;
+			   writer.resource_limits.max_samples_per_instance = 100;
+			   writer.deadline.period.sec = 100;
+			   writer.deadline.period.nanosec = 0;
             }
         }
     }
@@ -108,33 +165,70 @@ void SelfNego::getQoS()
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind = KEEP_LAST_HISTORY_QOS;
+				reader.history.depth=10;
+				reader.deadline.period.sec = 150;
+				reader.deadline.period.nanosec = 0;
+				reader.time_based-filter .minimum_separation.sec=10;
+				reader.time_based-filter .minimum_separation.naosec=0;
             }
             else if (isReader == 2)
             {
-
+				writer.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				writer.reliability.max_blocking_time.sec = 10;
+				writer.reliability.max_blocking_time.nanosec = 0;
+				writer.resource_limits.max_samples_per_instance = 100;
+				writer.history. kind = KEEP_LAST_HISTORY_QOS;
+				writer.history.depth=10;
+				writer.deadline.period.sec = 100;
+				writer.deadline.period.nanosec = 0;
+				writer.lifespan.duration.sec = 50;
+				writer.lifespan.duration.naosec = 0;
             }
         }
         else if (packetloss > PACKETLOSS_THRESHOLD_2 && packetloss <= PACKETLOSS_THRESHOLD_3)
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind = KEEP_LAST_HISTORY_QOS;
+				reader.history.depth=10;
+				reader.deadline.period.sec = 250;
+				reader.deadline.period.nanosec = 0;
+				reader.time_based-filter .minimum_separation.sec=50;
+				reader.time_based-filter .minimum_separation.naosec=0;
             }
             else if (isReader == 2)
             {
-
+				writer.history. kind = KEEP_LAST_HISTORY_QOS;
+				writer.history.depth=10;
+				writer.reliaability.kind = BEST_EFFORT_RELIABILITY_QOS;
+				writer.deadline.period.sec = 200;
+				writer.deadline.period.nanosec = 0;
+				writer.lifespan.duration.sec = 100;
+				writer.lifespan.duration.naosec = 0;
             }
         }
         else
         {
             if  (isReader == 1)
             {
-
+				reader.history. kind =KEEP_ALL_HISTORY_QOS;
+				reader.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+				reader.reliability.max_blocking_time.sec = 10;
+				reader.reliability.max_blocking_time.nanosec = 0;
+				reader.resource_limits.max_samples_per_instance = 100;
+				reader.deadline.period.sec     = 150;
+				reader.deadline.period.nanosec = 0;
             }
             else if (isReader == 2)
             {
-
+               writer.history.kind = KEEP_ALL_HISTORY_QOS; 
+			   writer.reliaability.kind = RELIABLE_RELIABILITY_QOS;
+			   writer.reliability.max_blocking_time.sec = 10;
+			   writer.reliability.max_blocking_time.nanosec = 0;
+			   writer.resource_limits.max_samples_per_instance = 100;
+			   writer.deadline.period.sec = 100;
+			   writer.deadline.period.nanosec = 0;
             }
         }
     }
