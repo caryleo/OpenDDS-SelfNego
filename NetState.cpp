78177839f47892e19003409d6eac057cfefe5674
JustianPupil <Justian@protonmail.com>
2018-05-02 11:58:55 +0800

    有bug，必须硬编码exe文件的绝对路径

--- NetState.cpp
+++ NetState.cpp
@@ -1,7 +1,8 @@
 #pragma
 
-
+#include "stdafx.h"
 #include "NetState.h"
+#include<stdlib.h>
 
 
 NetState::NetState() {
@@ -11,52 +12,30 @@ NetState::~NetState() {
 }
 
 void NetState::test(char ip[]) {
-	long bands[10];
-	GetSysInfo info;
-	int num = info.GetInterFaceCount(bands);
-	//printf("%d\n", num);
-	int best = 0;
-	for (int i = 0; i < num; i++) {
-		if (bands[i] > bands[best])
-			best = i;
-	}
-	setBandWidth(bands[best]);
-
-	CPing objPing;
-
-	char *szDestIP;
-	char defaultip[100] = "10.4.21.55";
-	szDestIP = ip;
-	if (ip == NULL)
-		szDestIP = defaultip;
-	PingReply reply;
-
-	//printf("Pinging %s with %d bytes of data:\n", szDestIP, DEF_PACKET_SIZE);
-	double times = 0;
-	int rolls = 100;
-	int faildpackegs = 0;
-	for (int i = 0; i < rolls; i++){
-		objPing.Ping(szDestIP, &reply, (DWORD)10);
-		if (reply.m_dwRoundTripTime > 50) {
-			faildpackegs++;
+	char cmd[100] = "C:\\Users\\Justian\\source\\repos\\NetState\\nestat.exe ";
+	strcat_s(cmd, ip);
+	FILE *pf = _popen(cmd, "r");
+	char res[1000] = { 0 };
+	fread(res, 1000, 1, pf);
+	_pclose(pf);
+	char re[4][100] = { 0 };
+	int k = 0;
+	int j = 0;
+	for (int i = 0; i < 1000; i++) {
+		if (res[i] == '\n' || res[i] == ' ' || res[i] == 37) {
+			j = 0;
+			k++;
+			if (res[i] == 37)
+				i++;
 			continue;
 		}
-		times += reply.m_dwRoundTripTime;
-		printf("\r[*]Network Testing[");
-		for (int j = 0; j + 1 < rolls / 2; j++) {
-			if (j < i / 2)
-				printf("#");
-			else
-				printf(".");
-		}
-		printf("]%d%%", i + 1);
-		//printf("Reply from %s: bytes=%ld time=%ldms TTL=%ld\n", szDestIP, reply.m_dwBytes, reply.m_dwRoundTripTime, reply.m_dwTTL);
-		//Sleep(500);
+		if (k == 4)
+			break;
+		re[k][j++] = res[i];
 	}
-	printf("\n[OK]Network test Finished!!!\n");
-	setDelay(times / (double)(rolls - faildpackegs));
-	setPacketLoss(((double)faildpackegs / rolls) * 100.0);
-	//setThroughput(DEF_PACKET_SIZE / delay);
+	setDelay(atof(re[1]));
+	setPacketLoss(atof(re[2]));
+	setBandWidth(atof(re[3]));
 }
 
 void NetState::setBandWidth(double input)
